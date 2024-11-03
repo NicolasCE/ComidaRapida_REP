aa36447542e98254c77b03609efc096db262b2e1
NicolasCE <nicontreras569@gmail.com>
2024-11-03 16:09:28 -0300

    segundo commit

--- ints.js
+++ ints.js
@@ -1,3 +1,8 @@
 function mostrarDetalles(nombre, precio) {
     alert(`Detalles del producto:\n\nProducto: ${nombre}\nPrecio: $${precio}`);
 }
+
+function toggleDetails(id) {
+    const element = document.getElementById(id);
+    element.classList.toggle('is-hidden');
+}
